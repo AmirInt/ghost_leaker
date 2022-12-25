b7d3f725ce1f69aa9b7c1a897486081b1a1b8a81
Amir-Int <amri.intelli@gmail.com>
2022-12-25 12:05:21 +0330

    simple-connection-setup finished

--- server.py
+++ server.py
@@ -10,7 +10,8 @@ def main():
         s.listen()
         conn, addr = s.accept()
         with conn:
-            
+            print(f"New connection made from {addr}")
+            print("Ghost says: '" + conn.recv(11).decode() + "'")
 
 if __name__ == "__main__":
     main()
